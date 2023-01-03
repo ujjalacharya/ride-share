0c81c20d639a245881cbf11f4101e04ff3305cf3
Utsav170 <stha24@gmail.com>
2023-01-03 12:52:31 +0100

    config google maps api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
